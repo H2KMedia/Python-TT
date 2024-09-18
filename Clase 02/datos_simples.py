77b0a8b0a9328e786a6a7729735905d98be06015
Esteban L <121070232+H2KMedia@users.noreply.github.com>
2024-09-17 21:00:53 -0300

    ejercicios iniciales en vsc

---
+++ Clase 02/datos_simples.py
@@ -0,0 +1,38 @@
+# comentario linea (numeral)
+
+'''
+comentario multilinea (triple comilla simple)
+'''
+
+'''
+atajo VSC
+
+Ctrl + K + C (multiple comentario)
+
+Ctrl + K + Q (descomentar multilinea)
+'''
+
+# variables
+# operador asignación (=) 
+# tipo de datos simples
+
+nombre = "Esteban" # string
+print(nombre)
+print(type(nombre))
+edad = 48 # int - entero
+print(edad)
+print(type(edad))
+peso = 123.500 #float - decimal
+print(peso)
+print(type(peso))
+altura = 1.96 # float - decimal
+print(altura)
+print(type(altura))
+ejercita = True #boolean - True / False
+print(ejercita)
+print(type(ejercita))
+
+print("--------------")
+imc = peso / (altura*altura)
+print(imc)
+print(type(imc))
