306082f58ac0a1822b99ccfb535e909623ce8eb9
Esteban L <121070232+H2KMedia@users.noreply.github.com>
2024-09-24 14:59:45 -0300

    agregados los ejercicios de la clase 02

---
+++ Clase 02/ejercicios_02.py
@@ -0,0 +1,38 @@
+'''
+EJERCICIO 1 - LISTA DE COMPRAS
+Usando la instrucción print() y lo visto en la clase,
+escribir un programa que muestre en la terminal la
+lista de productos que necesitamos comprar en el
+supermercado.
+'''
+titulo = "listado de productos a comprar en el super"
+print()
+print(titulo)
+print()
+print(46*"-")
+print("| Producto \t | Cantidad \t | Precio    | \t")
+print(46*"-")
+print("| Manteca \t |    1    \t | $1.490,00 | \t")
+print("| Aceite \t |    1    \t | $1.890,00 | \t")
+print("| Harina \t |    1    \t | $1.050,00 | \t")
+print("| Azúcar \t |    1    \t | $  980,00 | \t")
+print(46*"-")
+print("| Total calculado de compra  \t | $5.410,00 |")
+print(46*"-")
+print()
+print()
+print(60*"*")
+print()
+'''
+EJERCICIO 2 - INGRESO PROMEDIO
+Escribir un programa que guarde en variables el
+monto del ingreso de cada uno de los primeros 6
+meses del año.
+
+Luego, calcular y guardar en otra variable el
+promedio de esos valores.
+
+Por último, mostrar una leyenda que diga “El
+ingreso promedio en el semestre es de xxxxx”
+donde “xxxxx” es el valor calculado.
+'''
